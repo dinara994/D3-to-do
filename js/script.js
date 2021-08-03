deb5446ec37fd526d0b3fe8e7d7b28fcaee573b1
dinara994 <di_94@mail.ru>
2021-08-03 12:49:46 +0600

    initial

--- js/script.js
+++ js/script.js
@@ -3,28 +3,88 @@ const addBtn = document.querySelector('.add-btn')
 const clearAll = document.querySelector('.clear-all')
 const list = document.querySelector('.list')
 
- addBtn.addEventListener('click', () => { //
+
+function view() {
+    let todos = JSON.parse(localStorage.getItem('todos')) || []
+    let allTasks = ''
+    todos.forEach((item) => {
+        allTasks += `<li class="group-btn">
+        <input type="checkbox" class="done">
+            ${item.title}
+            <button class="del-btn">Удалить</button>
+    </li>`
+    })
+
+    list.innerHTML = allTasks
+    deleteActionItem()
+}
+
+
+view()
+
+addBtn.addEventListener('click', () => {
+    let todos = JSON.parse(localStorage.getItem('todos' || []))
+    todos = [...todos, {title: textInput.value, done: false}]
+    localStorage.setItem("todos", JSON.stringify(todos))
+    textInput.value = ''
+    view()
+})
+
+clearAll.addEventListener('click', () => {
+    localStorage.removeItem('todos')
+    view()
+})
+
+function deleteActionItem() {
+    const deleteButtons = document.querySelectorAll('.del-btn')
+    deleteButtons.forEach((btn, idx) => {
+
+        btn.addEventListener('ckick', () => {
+            let todos = JSON.parse(localStorage.getItem('todos' || []))
+            todos = [...todos, {title: textInput.value, done: false}]
+            localStorage.setItem("todos", JSON.stringify(todos))
+
+            view()
+        })
+
+    })
+}
+
+addBtn.addEventListener('click', () => { //
     if (textInput.value.trim()) { //убрать пробелы и пустую строку
-        const template = `<li class="group-btn">
-                            <input type="checkbox" class="done"> 
-                            ${textInput.value} 
+        const template = `<li class="flex">
+                            <div>
+                            <input type="checkbox" class="done">
+                            ${textInput.value}
+</div>
                             <button class="del-btn">Удалить</button>
                             </li>`
-        list.innerHTML += template //втавить HTML код
-        textInput.value = '' //Очистить инпут при добавление инф.
+        list.innerHTML += template
+        textInput.value = ''
+        // const template = `<li class="group-btn">
+        //                     <input type="checkbox" class="done">
+        //                     ${textInput.value}
+        //                     <button class="del-btn">Удалить</button>
+        //                     </li>`
+        // list.innerHTML += template //втавить HTML код
+        // textInput.value = '' //Очистить инпут при добавление инф.
     }
 })
+
+
 //------Enter
- textInput.addEventListener("keyup", (ev) => {
-     if (ev.keyCode === 13) {
-         const template = `<li>
-                            <input type="checkbox" class="done"> 
-                            ${textInput.value} 
+textInput.addEventListener("keyup", (ev) => {
+    if (ev.keyCode === 13) {
+        const template = `<li class="flex">
+                           <div>
+                            <input type="checkbox" class="done">
+                            ${textInput.value}
+</div>
                             <button class="del-btn">Удалить</button>
                             </li>`
-         list.innerHTML += template
-         textInput.value = ''
-     }
+        list.innerHTML += template
+        textInput.value = ''
+    }
 })
 clearAll.addEventListener("click", () => {
     list.innerHTML = ""
@@ -38,15 +98,11 @@ list.addEventListener('click', (event) => {
     }
 
 
-        if (event.target.classList[0] === "done") {
-            event.target.parentNode.classList.toggle("line-through")
-        }
+    if (event.target.classList[0] === "done") {
+        event.target.parentNode.classList.toggle("line-through")
+    }
 
 })
-
-
-
-
 
 
 // event - обьек хранит нажатие
@@ -60,20 +116,10 @@ list.addEventListener('click', (event) => {
 
 
 
-
-
-// label {
-//     position: relative;
-//     vertical-align: middle;
-//     cursor: pointer;
-//     padding: 18px 18px 18px 40px;
-//     display: block;
-// }
-
-    // ::-webkit-input-placeholder {
-    // color: #f2a899;
-    // font-size: 18px;
-    // opacity: 0.6;
+// ::-webkit-input-placeholder {
+// color: #f2a899;
+// font-size: 18px;
+// opacity: 0.6;
 
 
 
